3d5e173d9839291d6374239deb80c719fad08a3f
Aviprit Shah <avipritshah15@gmail.com>
2025-11-05 23:56:34 +0530

    added route.ts file inside api/gemini/chat

--- src/lib/api/gemini/chat/route.ts
+++ src/lib/api/gemini/chat/route.ts
@@ -8,7 +8,26 @@ export async function POST(request: Request) {
     if(url === undefined || apiKey === undefined) {
       return new Response('API configuration is missing', { status: 500 });
     }
-    const response = await axios.post(url, { prompt }, { headers: { 'Authorization': `Bearer ${apiKey}` } });
+    const response = await axios.post(url, 
+      {
+        "contents": [
+          {
+            "parts": [
+              {
+                "text": prompt
+              }
+            ]
+          }
+        ]
+      }, 
+      { 
+        headers: 
+        { 
+          'x-goog-api-key': `Bearer ${apiKey}`,
+          'Content-Type': 'application/json'
+        } 
+      }
+    );
     return new Response(JSON.stringify(response.data), { status: 200 });
   } catch (error) {
     console.error('Error:', error);
